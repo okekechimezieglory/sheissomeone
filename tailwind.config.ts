e2c0a2b2bd7e21ba0b874a42614afe95472f457f
okekechimezieglory <okekechimezieglory@gmail.com>
2024-12-11 09:31:44 -0800

    setup and fleshing

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,53 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
-export default {
+const config = {
+  darkMode: ['class'],
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}'
   ],
+  prefix: '',
   theme: {
+    container: {
+      center: true,
+      padding: '1rem'
+      
+    },
+    screens: {
+      sm: '660px',
+      md: '760px',
+      lg: '960px',
+      xl: '1200px'
+    },
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary: '#16213F',
+        secondary: '#050D28',
+        lighted: '#EFEFEF',
+        blue: {
+          DEFAULT: '#336DFF',
+          hover: '#336DFF'
+        }
       },
-    },
+      keyframes: {
+        'accordion-down': {
+          from: { height: '0' },
+          to: { height: 'var(--radix-accordion-content-height)' }
+        },
+        'accordion-up': {
+          from: { height: 'var(--radix-accordion-content-height)' },
+          to: { height: '0' }
+        }
+      },
+      animation: {
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out'
+      }
+    }
   },
-  plugins: [],
-} satisfies Config;
+  plugins: [require('tailwindcss-animate')]
+} satisfies Config
+
+export default config
